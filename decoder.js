1ebd0a6d922acca3fa5bc3f255f714d4d4454e1e
Kalebe <kalebemartins220@gmail.com>
2023-01-12 17:29:11 -0300

    place holder + background e + lógica de funcionamento

--- decoder.js
+++ decoder.js
@@ -6,17 +6,21 @@ cripto.addEventListener("click", function(e) {
     e.preventDefault();
 
     const texto = document.querySelector("#texto");
-    const codigo = document.querySelector("#codigo")
+    const codigo = document.querySelector("#codigo");
 
-    var value = texto.value.toLowerCase();
+    var value = texto.value;
 
-    var e = value.replace(/e/g, "enter");
+    if (value == value.toLowerCase()) {
+        var e = value.replace(/e/g, "enter");
     var i = e.replace(/i/g, "imes");
     var a = i.replace(/a/g, "ai");
     var o = a.replace(/o/g, "ober");
     var u = o.replace(/u/g, "ufat");
 
     codigo.innerHTML = u;
+    } else {
+        alert('Apenas letras minúsculas e sem acento')
+    }
 })
 
 descripto.addEventListener("click", function(e) {
@@ -33,13 +37,17 @@ descripto.addEventListener("click", function(e) {
     var o = a.replace(/ober/g, "o");
     var u = o.replace(/ufat/g, "u");
 
+    if (value != u) {
 
-    codigo.innerHTML = u;  
+    codigo.innerHTML = u;
+    } else {
+        alert('Descriptografia só funciona para textos criptografados ! ')
+    }
 })
 
 function copia(evento) {
     codigo.select();
-    codigo.setSelectionRange(0, 99999)
+    codigo.setSelectionRange(0, 99999);
     document.execCommand("copy");
 }
 
