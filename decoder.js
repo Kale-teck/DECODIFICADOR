081fd0e11744bf54521acaa6f33ddcaa2e7f0eb2
Kalebe <kalebemartins220@gmail.com>
2023-01-14 22:40:50 -0300

    apenas sem acentos e !charEsp funcionando
    Também block-resize nos textarea, limit input em 200chars, block write na caixa de copiar e start criação do footer com meus dados

--- decoder.js
+++ decoder.js
@@ -1,6 +1,23 @@
 const cripto = document.querySelector("#cripto");
 const descripto = document.querySelector("#descripto");
 const copiar = document.querySelector("#copiar");
+const texto = document.querySelector("#texto");
+
+texto.addEventListener("keypress", function(e) {
+    if(!checkChar(e)) {
+      e.preventDefault();
+  }
+});
+
+function checkChar(e) {
+    var char = String.fromCharCode(e.keyCode);
+  
+  console.log(char);
+    var pattern = '[a-zA-Z0-9]';
+    if (char.match(pattern)) {
+      return true;
+  }
+}
 
 cripto.addEventListener("click", function(e) {
     e.preventDefault();
